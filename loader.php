6089fbb155f0555277b684afa477e13c2717dec8
Ripul <ripul@99fusion.com>
2018-03-27 15:11:03 +0530

    loader

---
+++ loader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Vibe BuddyPress API
+ * Plugin URI:  https://vibethemes.com
+ * Description: BuddyPress Rest API for REACT
+ * Author:      VibeThemes
+ * Author URI:  https://vibethemes.com
+ * Version:     1.0
+ * Text Domain: vibe-bp-api
+ * Domain Path: /languages/
+ * License:     GPLv2 or later (license.txt)
+ */
